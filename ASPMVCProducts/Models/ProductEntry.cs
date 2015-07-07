2e8cc784bbb66c4cea69ca7265e48604cf208342
noone <noone@noone.com>
2015-07-07 15:34:19 +0200

    Added forgotten file

---
+++ ASPMVCProducts/Models/ProductEntry.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ASPMVCProducts.Models
+{
+	public class ProductEntry
+	{
+		[Key]
+		public int Id { get; set; }
+		[Key]
+		public ProductList List { get; set; }
+		public virtual Product Product { get; set; }
+		
+		public bool Checked { get; set; }
+		public int Ammount { get; set; }
+		public virtual List<ProductCategory> Categories { get; set; }
+		public string Comments { get; set; }
+
+	}
+}
